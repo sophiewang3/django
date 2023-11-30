4a0d9622d884f428cd04b3782055086cbcde11e6
sophiewang3 <sophie.wang@datadoghq.com>
2023-11-29 21:48:52 -0500

    add enricher file

--- django/core/files/images.py
+++ django/core/files/images.py
@@ -30,8 +30,6 @@ class ImageFile(File):
             self._dimensions_cache = get_image_dimensions(self, close=close)
         return self._dimensions_cache
 
-def add(x, y):
-    return x + y
 
 def get_image_dimensions(file_or_path, close=False):
     """
